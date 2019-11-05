2db2016b0e64462e4505e7b79200822cec81c4bc
Michał Lisak <michal.lisak@o2.pl>
2019-11-05 22:11:10 +0100

    Short documentation for crypto functions added

--- encrypt/encrypt.h
+++ encrypt/encrypt.h
@@ -11,6 +11,24 @@
 #define IV_LEN 12
 #define AEAD_TAG_LEN 16
 
+/* Wrappers for openssl's AES-256-GCM
+ *
+ * For both functions:
+ *
+ * in - input buffer
+ * in_len - length of input buffer
+ * key - buffer containing key bytes (32 bytes)
+ * iv - buffer containing iv bytes (12 bytes)
+ * out - output buffer
+ * aead - output buffer for AEAD tag (16 bytes)
+ *
+ * All the buffers should be managed (allocated and freed)
+ * by the caller.
+ *
+ * The functions return number of succesfully processed bytes,
+ * or negative number indicating an error (see defines above).
+ */
+
 int encrypt(const unsigned char* in, const int in_len,
             const unsigned char* key, const unsigned char* iv,
             unsigned char* out, unsigned char* aead);
