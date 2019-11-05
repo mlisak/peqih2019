1f8219ab46ced4c72d75c4a1c9f01d6a533c3d70
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-11-05 22:05:46 +0100

    both way functionality

---
+++ teleport_example/qkd.py
@@ -0,0 +1,64 @@
+from cqc.pythonLib import CQCConnection, qubit
+
+def main():    
+    # Egemevo --> SEND
+    # Cenkovich --> RECV
+    # device_name = 'Egemevo'
+    comm         = 'SEND'
+    while True:
+        if comm=='RECV':
+            recv_conn()
+        elif comm=='SEND':
+            create_conn()
+def create_conn(device_name='Egemevo',target_name='Cenkovich'):
+    secret_key = ''
+    with CQCConnection(device_name) as Alice:
+        for i in range(256):
+            # Make an EPR pair with Bob
+            qA = Alice.createEPR(target_name)
+            # Create a random number
+            q_random = qubit(Alice)
+            q_random.H()
+            r_number = q_random.measure()
+            secret_key += str(r_number)
+            #print('Random number: ' + str(r_number))
+            # Create a qubit to teleport
+            q = qubit(Egemevo)
+            # Prepare the qubit to teleport in |+>
+            if r_number==1:
+                q.X()
+            # Apply the local teleportation operations
+            q.cnot(qA)
+            q.H()
+            # Measure the qubits
+            a = q.measure()
+            b = qA.measure()
+            Egemevo.sendClassical("Cenkovich", [a, b])
+        print(hex(int(secret_key,2)))
+def recv_conn(device_name="Cenkovich"):
+    t0 = time.time()
+    # Initialize the connection
+    with CQCConnection(device_name) as Bob:
+        secret_key = ''
+        for i in range(256):
+            # Make an EPR pair with Alice
+            qB = Bob.recvEPR()
+            # Receive info about corrections
+            data = Bob.recvClassical()
+            message = list(data)
+            a = message[0]
+            b = message[1]
+            # Apply corrections
+            if b == 1:
+                qB.X()
+            if a == 1:
+                qB.Z()
+            # Measure qubit
+            m = qB.measure()
+            secret_key += str(m) 
+        print(hex(int(secret_key,2)))
+    t1 = time.time()
+    print('It took: ' + str(t1-t0) + ' seconds')
+
+main()
+
