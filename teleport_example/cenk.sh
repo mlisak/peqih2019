11bf8044f741146d1f07d4fde5b5244b0f0cda96
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-11-05 20:50:56 +0100

    minor change

--- teleport_example/cenk.sh
+++ teleport_example/cenk.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 simulaqron set network-config-file $(pwd)/cenk_network.json
-<<<<<<< HEAD
-simulaqron start --keep
-python3 aliceTest.py
-simulaqron stop
-=======
-simulaqron get network-config-file 
 simulaqron start --nodes Cenkovich --keep 
 python3 bobTest.py
 simulaqron stop 
->>>>>>> 5d364e5706729d7024c7ffa8e52867285179c50f
