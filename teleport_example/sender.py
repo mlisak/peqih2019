db9a1fc28df1dd9d91fcd98cee9ee03cdfa417ee
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-11-06 00:01:27 +0100

    one more one more minor change

--- teleport_example/sender.py
+++ teleport_example/sender.py
@@ -20,9 +20,9 @@ try:
     amount_expected = len(message)
 
     while amount_received <= amount_expected:
-        data = sock.recv(64)
+        data = sock.recv(66)
         amount_received += len(data)
-        if len(data)==64:
+        if len(data)==66:
             print(data.decode("utf-8"))
 
 finally:
