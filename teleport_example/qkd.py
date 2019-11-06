27856651f98a67bebc7687e170a49e2db1d23bf1
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-11-06 09:15:05 +0100

    minor changes

--- teleport_example/qkd.py
+++ teleport_example/qkd.py
@@ -11,17 +11,18 @@ def main():
         target_name = 'cenkmac'
     else:
         assert False, "Unspecified device name %s" % device_name
-
+        
     conn,comm = listen()
     if comm==b'RECV':
         recv_conn(conn,device_name)
     elif comm==b'SEND':
-        create_conn(conn,device_name, target_name)
+        create_conn(conn,device_name,target_name)
 
 def create_conn(conn,device_name='Egemevo',target_name='Cenkovich'):
-    print('Sending receive to target')
-    send_rec(device_name,target_name)
-    print('Starting creating and sending QK')
+    t0 = time.time()
+    print('Sending RECV from %s receive to %s' %(device_name,target_name))
+    #send_rec(device_name,target_name)
+    print('Starting creating and sending QK from: %s to: %s' %%(device_name,target_name) )
     secret_key = ''
     with CQCConnection(device_name) as Alice:
         for i in range(256):
@@ -46,12 +47,17 @@ def create_conn(conn,device_name='Egemevo',target_name='Cenkovich'):
             b = qA.measure()
             Alice.sendClassical(target_name, [a, b])
         print(hex(int(secret_key,2)))
-    send(conn,hex(int(secret_key,2)))
+    t1 = time.time()
+    secs = t1-t0
+    print('It took: %d mins %d seconds' % (secs // 60, secs % 60))
+    send(conn,hex(int(secret_key,2))) # Sending the result back
 def recv_conn(conn,device_name="Cenkovich"):
+    conn.close()
     print('Starting receiving QK')
     t0 = time.time()
     # Initialize the connection
     with CQCConnection(device_name) as Bob:
+        print('cenk')
         secret_key = ''
         for i in range(256):
             # Make an EPR pair with Alice
@@ -77,15 +83,12 @@ def recv_conn(conn,device_name="Cenkovich"):
 def listen():
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # Bind the socket to the port
-    server_address = ('0.0.0.0', 10000)
+    server_address = ('localhost', 10000)
     print('starting up on {} port {}'.format(*server_address))
     sock.bind(server_address)
-
     # Listen for incoming connections
     sock.listen(1)
-
     while True:
         # Wait for a connection
         print('waiting for a connection')
@@ -114,12 +117,10 @@ def send_rec(device_name,target_name):
     server_address = (target_ip, 10000)
     print('connecting to {} port {}'.format(*server_address))
     sock.connect(server_address)
-
     try:
         message = str.encode('RECV') 
         print('sending {!r}'.format(message))
         sock.sendall(message)
-
     finally:
         print('closing socket')
         sock.close()
